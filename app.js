f383d60073fd5196af01e1f5bc23384184b2b52e
Thomas Fajoux <tfajoux@gmail.com>
2015-09-07 23:29:53 +0200

    commit fin de journée

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 angular.module('myApp', ['ui.bootstrap','ngRoute']);
+
+angular.module('myApp')
+.config(function ($routeProvider){          
+    $routeProvider
+    .when("/421", {
+        templateUrl: "jeux/421/template/App421.html",
+        // controller: "App421Controller",
+        // controllerAs: "app421Ctrl"
+    })
+    .when("/morpion", {
+        templateUrl: "jeux/morpion/template/morpion.html",
+        // controller: "MorpionController",
+        // controllerAs: "morpionCtrl"
+    })
+    .when("/pendu", {
+      templateUrl: "jeux/pendu/template/pendu.html",
+        // controller: "PenduController",
+        // controllerAs: "penduCtrl"
+    })
+    .otherwise("/pendu");
+});
