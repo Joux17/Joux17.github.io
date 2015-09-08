56935f1c208700ecb33e02860464829acd0fda5d
Thomas Fajoux <tfajoux@gmail.com>
2015-09-08 23:23:45 +0200

    421 html + controller

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ angular.module('myApp')
     $routeProvider
     .when("/421", {
         templateUrl: "jeux/421/template/App421.html",
-        // controller: "App421Controller",
-        // controllerAs: "app421Ctrl"
+        controller: "App421Controller",
+        controllerAs: "app421Ctrl"
     })
     .when("/morpion", {
         templateUrl: "jeux/morpion/template/morpion.html",
-        // controller: "MorpionController",
-        // controllerAs: "morpionCtrl"
+        controller: "MorpionController",
+        controllerAs: "morpionCtrl"
     })
     .when("/pendu", {
       templateUrl: "jeux/pendu/template/pendu.html",
-        // controller: "PenduController",
-        // controllerAs: "penduCtrl"
+        controller: "PenduController",
+        controllerAs: "penduCtrl"
     })
-    .otherwise("/pendu");
+    .otherwise("/421");
 });
---
+++ jeux/421/App421Controller.js
@@ -0,0 +1,46 @@
+angular.module('myApp')
+.controller('App421Controller', function(){
+
+	var self=this;
+	var imgSrc = "http://lessac.escdijon.eu/uploads/images/";
+
+	self.des = [{value:6, img:imgSrc + "6.jpg",locked:false},{value:6, img:imgSrc + "6.jpg",locked:false},{value:6, img:imgSrc + "6.jpg",locked:false}];
+	self.nbLance = 0;
+
+	self.lancer = function(){//fonction pour lancer les dés
+		var newValue=0;
+		console.log("fonction lancer");
+		for(var i=0; i<3; i++){
+			if(!self.des[i].locked){
+				newValue=self.randomValue();
+				self.des[i].value = newValue;
+				self.des[i].img = imgSrc+newValue+".jpg";
+			}
+		}
+
+		if(self.des[0].locked && self.des[1].locked&& self.des[2].locked){
+			console.log(421);
+		}
+		else{
+			self.nbLance++;
+		}
+
+	};
+
+	self.randomValue = function(){ //tirer des dés aleatoirement
+		return Math.floor((Math.random()*6)+1);
+	};
+
+	self.lock = function(idDe){
+		self.des[idDe].locked = !self.des[idDe].locked;
+	};
+
+
+	self.reset = function(){
+		self.des = [{value:'6', img:imgSrc + "6.jpg",locked:false},{value:'6', img:imgSrc + "6.jpg",locked:false},{value:'6', img:imgSrc + "6.jpg",locked:false}];
+		self.nbLance = 0;
+	};
+
+
+	
+});
