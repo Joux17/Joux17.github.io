3c30a65b35701249ea016d61a389b26382c96f44
Thomas Fajoux <tfajoux@gmail.com>
2015-10-18 21:09:27 +0200

    modif

--- jeux/421/App421Controller.js
+++ jeux/421/App421Controller.js
@@ -1,26 +1,40 @@
 angular.module('myApp')
-.controller('App421Controller', function(){
+.controller('App421Controller', function($interval){
 
 	var self=this;
 	var imgSrc = "http://lessac.escdijon.eu/uploads/images/";
 
 	self.des = [{value:6, img:imgSrc + "6.jpg",locked:false},{value:6, img:imgSrc + "6.jpg",locked:false},{value:6, img:imgSrc + "6.jpg",locked:false}];
 	self.nbLance = 0;
+	self.nbAnim=0;
 
-	  console.log("controller app421");
+	self.effetLancer = function()
+	{
+		if(self.nbAnim<12){
+			var newValue=0;
+			for(var i=0; i<3; i++){
+				if(!self.des[i].locked){
+					newValue=self.randomValue();
+					self.des[i].value = newValue;
+					self.des[i].img = imgSrc+newValue+".jpg";
+				}
+			}
+			self.nbAnim++;
+		}
+		else{
+			$interval.cancel(self.boucle);
+			self.boucle = undefined;
+			self.nbAnim=0;
+
+		}
+	}
 
 	self.lancer = function(){//fonction pour lancer les dés
-		var newValue=0;
-		console.log("fonction lancer");
-		for(var i=0; i<3; i++){
-			if(!self.des[i].locked){
-				newValue=self.randomValue();
-				self.des[i].value = newValue;
-				self.des[i].img = imgSrc+newValue+".jpg";
-			}
+		if(!self.des[0].locked || !self.des[1].locked || !self.des[2].locked){
+			self.boucle = $interval(self.effetLancer, 100);
 		}
 
-		if(self.des[0].locked && self.des[1].locked&& self.des[2].locked){
+		if(self.des[0].locked && self.des[1].locked && self.des[2].locked){
 			console.log(421);
 		}
 		else{
@@ -41,6 +55,7 @@ angular.module('myApp')
 	self.reset = function(){
 		self.des = [{value:'6', img:imgSrc + "6.jpg",locked:false},{value:'6', img:imgSrc + "6.jpg",locked:false},{value:'6', img:imgSrc + "6.jpg",locked:false}];
 		self.nbLance = 0;
+		self.nbAnim=0;
 	};
 
 
--- jeux/pendu/penduController.js
+++ jeux/pendu/penduController.js
@@ -1,14 +1,31 @@
 angular.module('myApp')
     .controller('PenduController', function(){
         var self = this;
+
         self.imgSrc="http://www.pour-enfants.fr/jeux-vacances/dessins-jeu-pendu/dessin-jeu-pendu-0.gif";
-        self.mot="";
+        self.mot='';
         self.check=false;
+        self.tabMot = [];
+        self.lettre ='';
+        self.afficher = false;
 
         self.soummetreMot = function(mot){
             self.mot=mot;
             self.check=true;
-            console.log(self.check);
+            console.log(self.mot);
+            for(var i=0; i < mot.length; i++){
+                self.tabMot.push({letter : mot.charAt(i), visible : false});
+            }
+        };
+
+        self.essai = function(lettre){
+            console.log("essai");
+            for(var i=0; i<self.tabMot.length;i++){
+                if(lettre===self.tabMot[i].letter){
+                    self.tabMot[i].visible = true;
+                    console.log("lettre trouvée");
+                }
+            }
         };
 
         //TODO
